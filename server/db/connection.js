b030e4e2641d5643a608b4233bc58c3ae6a7b246
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-17 23:27:47 +0200

    fixata variabile sb

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,7 +5,7 @@ dotenv.config();
 import mysql from "mysql2";
 
 const credentials = {
-  host: process.env.DB_A,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
@@ -18,13 +18,6 @@ console.log(connection);
 
 connection.connect((err) => {
   if (err) {
-    console.log(
-      process.env.DB_HOST,
-      process.env.DB_PORT,
-      process.env.DB_USER,
-      process.env.DB_PW,
-      process.env.DB_NAME
-    );
     console.log(err.message);
   } else {
     console.log("Connection Success");
