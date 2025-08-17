ab11ba3b2613cc2d5fe552d1ce701399555cd9a8
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-17 23:19:03 +0200

    inserite nuove variabili per la conessione al db di railway

--- server/db/connection.js
+++ server/db/connection.js
@@ -5,7 +5,8 @@ dotenv.config();
 import mysql from "mysql2";
 
 const credentials = {
-  host: process.env.DB_H,
+  host: process.env.DB_A,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PW,
   database: process.env.DB_NAME,
@@ -17,6 +18,13 @@ console.log(connection);
 
 connection.connect((err) => {
   if (err) {
+    console.log(
+      process.env.DB_HOST,
+      process.env.DB_PORT,
+      process.env.DB_USER,
+      process.env.DB_PW,
+      process.env.DB_NAME
+    );
     console.log(err.message);
   } else {
     console.log("Connection Success");
