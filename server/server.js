3921dbe06174fd43d5a8a8b6a1bbd0617045e4ed
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-17 20:37:17 +0200

    preparazione per deploy

--- server/server.js
+++ server/server.js
@@ -8,13 +8,23 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
+
 const app = express();
+
 app.use(cors());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use("/gym", gymRouter);
 
+// Serve frontend Vite (file statici)
+app.use(express.static(path.join(__dirname, "dist")));
+
+// Catch-all per React Router
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 app.listen(port, () => {
-  console.log(`Sto ascoltando il server alla porta http://localhost:${port}`);
+  console.log(`Server in ascolto su http://localhost:${port}`);
 });
