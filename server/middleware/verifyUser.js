478e17b1b63f91d8d3034977085e7497935fc403
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-18 09:46:29 +0200

    elimanta cartella server

--- server/middleware/verifyUser.js
+++
@@ -1,7 +0,0 @@
-// Middleware extra: verifica ruolo admin
-export default function verifyUser(req, res, next) {
-  if (req.user.role !== "user") {
-    return res.status(403).json({ message: "Accesso riservato agli utenti" });
-  }
-  next();
-}
