bc7cf3729e31da064a82fc12864e29914a6d4468
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-19 13:51:53 +0200

    fix

--- src/components/ShowProfile.jsx
+++ src/components/ShowProfile.jsx
@@ -11,8 +11,10 @@ export default function ShowProfile(prop) {
   console.log(dataUser.id);
   function viewScheda() {
     const token = localStorage.getItem("token");
-
-    fetch(`${import.meta.env.VITE_URL_GET_SCHEDA}${dataUser.id}`, {
+    console.log(
+      `${import.meta.env.VITE_URL_GET_SCHEDA}${dataUser.id_iscritto}`
+    );
+    fetch(`${import.meta.env.VITE_URL_GET_SCHEDA}${dataUser.id_iscritto}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((res) => {
