1f6a8445093fd7554b36b19e2e6d0a949a9c24fd
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-19 16:13:58 +0200

    importato Navlink

--- src/components/MainAdmin.jsx
+++ src/components/MainAdmin.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"; // per fare richieste  http al server
-
+import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Jumbo from "./JumboC";
