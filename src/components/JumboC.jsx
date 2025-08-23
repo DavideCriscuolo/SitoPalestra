673a0857955e14c35631dec81fe39424c7f35c8f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-23 16:14:58 +0200

    aggiunto video per nel jumbo

--- src/components/JumboC.jsx
+++ src/components/JumboC.jsx
@@ -2,14 +2,26 @@ import "./../css/Jumbo.scss";
 
 export default function Jumbo() {
   return (
-    <div className="container-fluid">
-      <div className=" jumbo px-sm-3 mb-4 bg-light rounded-3  my-5 d-flex align-items-end ">
-        <div className="container-fluid  text-white">
-          <h1 className="fw-bold">PALASPORTMAX</h1>
-          <p className="col-md-8 fs-4 fst-italic">
-            COSTRUISCI LA TUA FORZA. SUPERA I TUOI LIMITI. DIVENTA LA TUA
-            VERSIONE MIGLIORE.
-          </p>
+    <div class=" mb-4 rounded-3">
+      <div class="container-fluid containerVideo position-relative top-0 start-0  d-inline-flex flex-column justify-content-end p-2 ">
+        <video
+          className="video rounded-3  position-absolute top-0 p-1  start-0 w-100 "
+          autoPlay
+          muted
+          loop
+          src="./img/Video_jumbo.mp4"
+        ></video>
+        <div className="p-1">
+          <div>
+            {" "}
+            <h1 className="text-white fw-bold m-0 p-xl-1">PALASPORTMAX</h1>{" "}
+          </div>
+          <div>
+            <p className="text-white lh-sm  m-0 py-1 p-xl-2">
+              Costruisci la tua forza. Supera i tuoi limiti. Diventa la tua
+              versione migliore.{" "}
+            </p>
+          </div>
         </div>
       </div>
     </div>
