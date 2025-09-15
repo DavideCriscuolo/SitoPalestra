b978e7b54220cdca10ddf2e159266d303568f5f8
DavideCriscuolo <davide.alcatel@gmail.com>
2025-09-15 21:11:57 +0200

    aggiunto children nel componenete animated eper animare tutte le pagine

--- src/components/Animate.jsx
+++ src/components/Animate.jsx
@@ -1,12 +1,13 @@
 import { AnimatePresence, motion } from "framer-motion";
-
+import { useLocation } from "react-router-dom";
 export default function Animate({ children }) {
+  const location = useLocation();
   return (
     <AnimatePresence>
       <motion.div
+        key={location.pathname}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
         transition={{ duration: 1 }}
       >
         {children}
